eaab28d7735b1f59ff1fb9a107c0b1bbfb2ebb1f
Leandro Orieta <lorieta@tupaca.com>
2024-10-18 07:57:03 -0300

    DEVOPS-21 - refactor

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,25 +1,6 @@
 import { RedisOptions } from "ioredis";
 import { Options } from "nodemailer/lib/smtp-pool";
 
-export interface TransportOptions {
-  host?: string;
-  port?: number;
-  secure?: boolean;
-  auth: {
-    type?: string;
-    user: string;
-    pass: string;
-    clientId?: string;
-    clientSecret?: string;
-    refreshToken?: string;
-  };
-  pool?: boolean;
-  maxConnections?: number;
-  maxMessages?: number;
-  rateLimit?: number;
-  service?: string;
-}
-
 export interface MailerConfig {
   nodemailer: Options;
   redis: RedisOptions;
